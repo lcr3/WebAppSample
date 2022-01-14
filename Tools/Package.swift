0b1e188af4b5351d4f17c836975036738a36fa46
lcr <llcoolryo@gmail.com>
2022-01-14 12:55:01 +0900

    Add Tools

---
+++ Tools/Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "Tools",
+    products: [
+    ],
+    dependencies: [
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.48.18"),
+    ],
+    targets: [
+    ]
+)
